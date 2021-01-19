6dd5f10727bb2f04a7591ea9cf70395503ba31a0
alexprod2015 <alexproductions2015@gmail.com>
2021-01-19 22:06:15 +0000

    20. Manual tank movement

--- BattleTanks/Source/BattleTanks/TankMovementComponent.cpp
+++ BattleTanks/Source/BattleTanks/TankMovementComponent.cpp
@@ -1,10 +1,25 @@
 // Alex Rusu 2021
 
-
+#include "TankTrack.h"
 #include "TankMovementComponent.h"
+
+void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+    LeftTrack = LeftTrackToSet;
+    RightTrack = RightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-    // auto Time = GetWorld()->GetTimeSeconds();
-	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);	
+    if (!LeftTrack || !RightTrack) {return;}
+    LeftTrack->SetThrottle(Throw);
+    RightTrack->SetThrottle(Throw);
+}
+
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
+    if (!LeftTrack || !RightTrack) {return;}
+    LeftTrack->SetThrottle(Throw*2);
+    RightTrack->SetThrottle(-Throw*2);
+    // TODO prevent double speed
 }
