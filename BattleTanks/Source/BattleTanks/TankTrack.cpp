600aad86f07aaf11c8a681a3f21784ea9cf9504c
alexprod2015 <alexproductions2015@gmail.com>
2021-01-25 00:52:56 +0000

    26. Movement improvements? Avoiding boolean flags

--- BattleTanks/Source/BattleTanks/TankTrack.cpp
+++ BattleTanks/Source/BattleTanks/TankTrack.cpp
@@ -3,11 +3,46 @@
 
 #include "TankTrack.h"
 
+UTankTrack::UTankTrack()
+{
+    PrimaryComponentTick.bCanEverTick = false;
+
+}
+void UTankTrack::BeginPlay()
+{
+        Super::BeginPlay();
+	OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
+}
+
+void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
+{
+    DriveTrack();
+    ApplySidewaysForce();
+    CurrentThrottle = 0;
+}
+
 void UTankTrack::SetThrottle(float Throttle)
 {
+    CurrentThrottle = FMath::Clamp<float>(CurrentThrottle + Throttle, -1.f, 1.f);
+}
+
+void UTankTrack::DriveTrack()
+{
     // TODO clamp throttle value so player can't override
-    auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+    auto ForceApplied = GetForwardVector() * CurrentThrottle * TrackMaxDrivingForce;
     auto ForceLocation = GetComponentLocation();
     auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
     TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+}
+
+void UTankTrack::ApplySidewaysForce()
+{
+    auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+    auto DeltaTime = GetWorld()->GetDeltaSeconds();
+    auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
+
+    // Calculate and apply sideways force (F = m a)
+    auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+    auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2; //Two tracks
+    TankRoot->AddForce(CorrectionForce);
 }
--- BattleTanks/Source/BattleTanks/TankTrack.h
+++ BattleTanks/Source/BattleTanks/TankTrack.h
@@ -22,4 +22,15 @@ public:
 	UPROPERTY(EditDefaultsOnly)
 	float TrackMaxDrivingForce = 40000000.f; // Assume 40 tank, 1g acceleration
 	
+private:
+	UTankTrack();
+
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+	
+	void ApplySidewaysForce();
+	void DriveTrack();
+	float CurrentThrottle = 0;
 };
